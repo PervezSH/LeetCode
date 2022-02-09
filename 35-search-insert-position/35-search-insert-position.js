cc807983bf2bb4f9f546fd86507a0f3feaf781cf
Pervez Shoaib <50441829+PervezSH@users.noreply.github.com>
2022-02-09 15:05:18 +0530

    Time: 71 ms (82.53%), Space: 41.9 MB (17.85%) - LeetHub

---
+++ 35-search-insert-position/35-search-insert-position.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var searchInsert = function(nums, target) {
+    let lo = 0
+    let hi = nums.length - 1
+    while (lo <= hi) {
+        let mid = Math.floor((lo + hi)/2)
+        let midVal = nums[mid]
+        if (target === midVal) return mid
+        else if (target > midVal) lo = mid + 1
+        else hi = mid - 1
+    }
+    return lo
+};
