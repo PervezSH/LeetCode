ddb11fab333d50855dfb9de344c0a43580e1cea8
Pervez Shoaib <50441829+PervezSH@users.noreply.github.com>
2022-04-27 16:38:58 +0530

    Time: 0 ms (100.00%), Space: 2.6 MB (8.13%) - LeetHub

--- 1-two-sum/1-two-sum.rs
+++ 1-two-sum/1-two-sum.rs
@@ -1,5 +1,6 @@
 use std::collections::HashMap;
 impl Solution {
+    // O(n) time | O(n) space - where n is length of the array
     pub fn two_sum(nums: Vec<i32>, target: i32) -> Vec<i32> {
         let mut differences: HashMap<i32, usize> = HashMap::new();
         for (pos, num) in nums.iter().enumerate() {
