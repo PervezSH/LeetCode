f8e6fcebde2d26c5f4909626838a813d30f78dd8
Pervez Shoaib <50441829+PervezSH@users.noreply.github.com>
2022-04-27 16:36:06 +0530

    Time: 2 ms (70.02%), Space: 2.6 MB (18.09%) - LeetHub

---
+++ 1-two-sum/1-two-sum.rs
@@ -0,0 +1,13 @@
+use std::collections::HashMap;
+impl Solution {
+    pub fn two_sum(nums: Vec<i32>, target: i32) -> Vec<i32> {
+        let mut differences: HashMap<i32, usize> = HashMap::new();
+        for (pos, num) in nums.iter().enumerate() {
+            match differences.get(num) {
+                Some(i) => return vec![*i as i32, pos as i32],
+                None => differences.insert(target - num, pos),
+            };
+        }
+        vec![]
+    }
+}
